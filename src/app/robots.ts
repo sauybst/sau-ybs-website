020ba996dbefe0a4219afe572f40981583633c33
ErsinGPT <ersingorun@gmail.com>
2026-03-19 00:25:23 +0300

    fix: Correct base URL in robots.ts and update rules format

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,15 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://www.sauybst.com'
+    const baseUrl = 'https://sauybst.com'
 
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
         sitemap: `${baseUrl}/sitemap.xml`,
     }
 }
